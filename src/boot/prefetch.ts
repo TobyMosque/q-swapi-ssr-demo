61b59ec12c75e6b3e4e7ae57c03349bf4dfb807d
Tobias Mesquita <tobias.mesquita@gmail.com>
2024-05-16 10:45:58 -0300

    splash screen

--- src/boot/prefetch.ts
+++ src/boot/prefetch.ts
@@ -4,7 +4,8 @@ import { enabled } from 'src/composables/prefetch';
 export default boot(({ router }) => {
   if (process.env.CLIENT) {
     router.isReady().then(() => {
-      document.body?.classList.remove('splash-screen');
+      const splashScreen = document.querySelector('.splash-screen');
+      splashScreen?.remove();
       enabled.value = true;
     });
   }
